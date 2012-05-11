c91fc153db3081e36576e0fbffba27229c649ed5
Joey Kasnicki <jkasnicki@gmail.com>
2012-05-10 22:25:56 -0500

    Make configure_system.sh safe on existing systems

--- configure_system.sh
+++ configure_system.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
-cp configs/dot_bash_aliases ~/.bash_aliases
-cp configs/dot_vimrc ~/.vimrc
+
+if [ -f ~/.bash_aliases ]; then
+  echo "Not overwriting bash_aliases"
+else
+  cp configs/dot_bash_aliases ~/.bash_aliases
+fi
+
+if [ -f ~/.vimrc ]; then
+  echo "Not overwriting vimrc"
+else
+  cp configs/dot_vimrc ~/.vimrc
+fi
