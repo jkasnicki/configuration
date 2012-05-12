0a3a63351f0f3c5131530ff7d75001452bc86200
Joey Kasnicki <jkasnicki@gmail.com>
2012-05-11 23:06:21 -0500

    GAE install script

---
+++ install/google_appengine.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# See: https://developers.google.com/appengine/downloads
+
+VERSION=1.6.5
+
+if [ -d /opt/google_appengine ]; then
+  echo "ERROR: Not overwriting existing installation"
+  exit 1
+fi
+
+cd /tmp
+wget http://googleappengine.googlecode.com/files/google_appengine_${VERSION}.zip
+
+unzip google_appengine_${VERSION}.zip
+sudo mv google_appengine /opt/
+
+grep ${VERSION} /opt/google_appengine/VERSION && echo "SUCCESS"
